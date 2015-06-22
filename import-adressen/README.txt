c58dc4b8affcac03093ee1797f9077e177489482
Alexander Kruth <kruth@bfpi.de>
2015-06-22 15:17:04 +0200

    Adressenimport

---
+++ import-adressen/README.txt
@@ -0,0 +1,6 @@
+Dieses Skript verwendet die als Parameter übergebene Adressdatei,
+um die Standortsuche-DB mit Ort, Straßen und Adressen zu füllen.
+
+Stadtteile müssen schon vorhanden sein.
+
+Die Geo-Koordinaten für die Straßen werden aus den Adressen interpoliert.
---
+++ import-adressen/import-adressen.rb
@@ -0,0 +1,57 @@
+#!/usr/bin/env ruby
+
+def load_config
+  require 'yaml'
+  YAML.load(File.open("config.yml"))
+end
+
+input = ARGV[0].to_s
+raise "Usage: #{ $0 } input/adressen.txt" unless File.exists?(input)
+
+config = load_config
+
+sql = <<SQL
+DROP TABLE IF EXISTS import_adressen;
+
+CREATE TABLE import_adressen (
+  nba CHAR (1), oi CHAR(16), qua CHAR(1), lan CHAR(2), rbz CHAR(1), krs CHAR(2),
+  gmd CHAR(3), ott CHAR(4), sss CHAR(5), hnr VARCHAR(100), adz VARCHAR(100),
+  eee CHAR(12), nnn CHAR(12), stn VARCHAR(255), plz CHAR(5), 
+  onm VARCHAR(255), zon VARCHAR(255), pot VARCHAR(255), str VARCHAR(255), void VARCHAR(1)
+);
+
+COPY import_adressen 
+  FROM '#{ File.absolute_path(input) }'
+  CSV 
+  DELIMITER ';' 
+  ENCODING 'LATIN1';
+
+TRUNCATE ort, adresse, strasse RESTART IDENTITY;
+
+INSERT INTO ort(name)
+SELECT DISTINCT onm FROM import_adressen;
+
+INSERT INTO strasse(name, ortsteil_id)
+SELECT DISTINCT stn, ot.id from import_adressen a INNER JOIN ortsteil ot ON ot.name = a.pot INNER JOIN ort o ON a.onm = o.name;
+
+INSERT INTO adresse(strasse_id, hausnummer, hausnummerzusatz, geom)
+SELECT
+  s.id, hnr::INTEGER, adz, ST_SetSRID(ST_MakePoint(
+    REPLACE(eee, ',', '.')::FLOAT/1000 - 33000000,
+    REPLACE(nnn, ',', '.')::FLOAT/1000
+  ), 25833)
+  FROM import_adressen a 
+    INNER JOIN strasse s ON s.name = a.stn
+    INNER JOIN ortsteil ot ON ot.name = a.pot
+    INNER JOIN ort o ON a.onm = o.name;
+
+UPDATE strasse SET geom = sub.geom FROM (
+  SELECT s.id, st_makeline(a.geom) AS geom
+  FROM strasse s
+  INNER JOIN adresse a ON a.strasse_id = s.id
+  GROUP BY s.id
+) sub WHERE sub.id = strasse.id;
+SQL
+
+puts `psql #{ config["database"] } <<SQL
+#{ sql }`
