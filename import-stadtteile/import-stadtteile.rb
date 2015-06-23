b372dff485e6825bb915939823d7ba38fe6ff24c
Alexander Kruth <kruth@bfpi.de>
2015-06-23 10:04:15 +0200

    SRID beim Umwandeln schon angeben

--- import-stadtteile/import-stadtteile.rb
+++ import-stadtteile/import-stadtteile.rb
@@ -13,14 +13,15 @@ config["targets"].each do |c|
   drop_table = %{psql -c 'DROP TABLE IF EXISTS "#{ config["import_table_name"] }"' #{ c["database"] }}
   `#{ drop_table }`
 
-  `shp2pgsql -W LATIN1 #{ shp_file } "#{ config["import_table_name"] }" | psql #{ c["database"]}`
+  `shp2pgsql -W LATIN1 -s 25833 #{ shp_file } "#{ config["import_table_name"] }" | psql #{ c["database"]}`
 
   # DELETE FROM, damit Trigger greifen und TRUNCATE um die Sequenz zurück zu setzen 
   puts `psql #{ c["database"]} <<SQL
   DELETE FROM "#{ c["table_name"] }";
   TRUNCATE "#{ c["table_name"] }" RESTART IDENTITY;
   INSERT INTO "#{ c["table_name"] }" (id, name, "#{ c["column_name"] }")
-  SELECT gid, "#{ config["shp_column_name"] }", ST_TRANSFORM(ST_SETSRID(geom, 2398), 25833) FROM "#{ config["import_table_name"] }";
+    SELECT gid, "#{ config["shp_column_name"] }", geom
+    FROM "#{ config["import_table_name"] }";
 SQL`
   `#{ drop_table }`
 end
